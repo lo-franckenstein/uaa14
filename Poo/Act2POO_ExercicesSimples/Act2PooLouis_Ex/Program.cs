4bdc3f0be2d211b622ed5ad102371236e025c687
lo-franckenstein <115708090+lo-franckenstein@users.noreply.github.com>
2023-09-19 14:33:34 +0200

    Ajout du travail non fini sur les exercices simples en POO

---
+++ Poo/Act2POO_ExercicesSimples/Act2PooLouis_Ex/Program.cs
@@ -0,0 +1,15 @@
+﻿uint exercice;
+float rayon;
+
+
+Console.WriteLine("Bonjour, quel exercice souhaitez-vous tester? \n[1] Cercle\n[2] \n[3]");
+exercice = uint.Parse(Console.ReadLine());
+
+if (exercice == 1)
+{
+    Console.Clear();
+    Console.WriteLine("Bienvenue dans ce programme sur le cercle !\nTapez un rayon pour votre cercle.");
+
+    rayon = float.Parse(Console.ReadLine());
+
+}
---
+++ Poo/Act2POO_ExercicesSimples/Act2PooLouis_Ex/cercle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act2PooLouis_Ex
+{
+    class cercle
+    {
+        private double _rayon;
+    }
+
+
+    class cercleClasse (double rayon)
+    {
+        _rayon = rayon;
+    }
+}
