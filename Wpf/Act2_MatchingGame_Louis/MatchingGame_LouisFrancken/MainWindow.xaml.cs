8fc2e5ab90246764e1a291ebe716db68040931f7
lo-franckenstein <115708090+lo-franckenstein@users.noreply.github.com>
2023-09-22 14:38:58 +0200

    Ajout du travail presque fini sur le MEMO WPF

---
+++ Wpf/Act2_MatchingGame_Louis/MatchingGame_LouisFrancken/MainWindow.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace MatchingGame_LouisFrancken
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        TextBlock derniereTBClique; // on va l’utiliser pour faire une référence à  la TextBlock sur laquelle on vient de cliquer
+        bool trouvePaire = false;
+
+        DispatcherTimer timer = new DispatcherTimer();
+        int tempsEcoule = 0;
+        int nbPairesTrouvees = 0;
+
+
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            timer.Interval = TimeSpan.FromSeconds(.1);
+
+        }
+        private void SetUpGame()
+        {
+            int index;
+            Random nbAlea = new Random();
+            string nextEmoji;
+
+
+
+
+            List<string> animalEmoji = new List<string>()
+            {
+            "🐈","🐈",
+            "🐷","🐷",
+            "🐐","🐐",
+            "🦊","🦊",
+            "🐴","🐴",
+            "🦨","🦨",
+            "🦉","🦉",
+            "🐀","🐀",
+            };
+            foreach (TextBlock textBlock in grdMain.Children.OfType<TextBlock>())
+            {
+                if (textBlock.Name != "txtTemps")
+                {
+                    index = nbAlea.Next(animalEmoji.Count);
+                    nextEmoji = animalEmoji[index]; 
+                    textBlock.Text = nextEmoji;
+                    animalEmoji.RemoveAt(index);
+                }
+            }
+
+            foreach (TextBlock textBlock in grdMain.Children.OfType<TextBlock>())
+            {
+
+                index = nbAlea.Next(animalEmoji.Count); // index est de type int
+                                                        // nbalea est un objet de type Random()
+                nextEmoji = animalEmoji[index]; // nextEmoji est de type string
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index); // on retire un animal de la liste pour ne pas l’attribuer à nouveau.
+            }
+
+
+
+
+            tempsEcoule = 0;
+            nbPairesTrouvees = 0;
+            timer.Start();
+        }
+
+
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlockActif = sender as TextBlock;
+            if (!trouvePaire)
+            {
+                textBlockActif.Visibility = Visibility.Hidden;
+                derniereTBClique = textBlockActif;
+                trouvePaire = true;
+            }
+            else if (textBlockActif.Text == derniereTBClique.Text)
+            {
+                textBlockActif.Visibility = Visibility.Hidden;
+                trouvePaire = false;
+            } else if (textBlockActif.Text == derniereTBClique.Text)
+            {
+                nbPairesTrouvees++;
+                textBlockActif.Visibility = Visibility.Hidden;
+                trouvePaire = false;
+            }
+            else
+            {
+                derniereTBClique.Visibility = Visibility.Visible;
+                trouvePaire = false;
+            }
+        }
+
+
+
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tempsEcoule++;
+            txtTemps.Text = (tempsEcoule / 10F).ToString("0.0s");
+            if (nbPairesTrouvees == 8)
+            {
+                timer.Stop();
+                txtTemps.Text = txtTemps.Text + " - Rejouer ? ";
+            }
+        }
+
+
+
+
+        private void txtTemps_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (nbPairesTrouvees == 8)
+            {
+                SetUpGame();
+            }
+        }
+
+
+
+
+    }
+}
