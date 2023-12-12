a6cd466e23c73ffcd354a374456952c7fd61ac4d
lo-franckenstein <115708090+lo-franckenstein@users.noreply.github.com>
2023-12-12 15:18:51 +0100

    Ajout de l'interrogation certificative

---
+++ Poo/EvaluationCertificative12-12/CeUAA14Partie2_dec23_FranckenLouis/GunFiles.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CeUAA14Partie2_dec23_FranckenLouis
+{
+    class GunFiles
+    {
+		public int _ballReserve;
+		public int _ballGun;
+		public bool _recharge;
+		private int _quantiteChargeur;
+		private int _quantiteDebut;
+
+
+		// Constructeur
+
+
+
+		
+
+		public int BallReservee
+		{
+			get { return _ballReserve; }
+			set { _ballReserve = value; }
+		}
+
+		public int BallDansGun
+		{
+			get { return _ballGun; }
+			set { _ballGun = value; }
+		}
+
+        public bool Chargement
+        {
+            get { return _recharge; }
+            set { _recharge = value; }
+        }
+
+
+        public int QuantitéChargeuur
+		{
+			get { return _quantiteChargeur; }
+		}
+
+        public int QuantitéDébut
+        {
+            get { return _quantiteDebut; }
+        }
+
+        public GunFiles(int ballReserve, int ballGun, bool recharge, int quantiteChargeur, int quantiteDebut)
+		{
+			_ballReserve = ballReserve;
+			_ballGun = ballGun;
+			_recharge = recharge;
+			_quantiteChargeur = quantiteChargeur;
+			_quantiteDebut = quantiteDebut;
+		}
+
+		public bool Tirer(int _ballGun)
+		{
+			_ballGun = _ballGun - 1;
+			return true;
+		}
+
+		public bool Recharger(int _ballGun, int _ballReserve, int _quantiteChargeur)
+		{
+			if (_quantiteChargeur <= _ballReserve)
+			{
+				_ballGun = _quantiteChargeur;
+				_ballReserve = _ballReserve - _quantiteChargeur;
+			} else
+			{
+				_ballGun = _ballReserve;
+				_ballReserve = 0;
+			}
+			return true;
+		}
+
+
+		public string AffichageStuff(int _ballReserve, int _ballGun, bool _recharge)
+		{
+			string messageRecharge;
+			if (_recharge == true)
+			{
+				messageRecharge = "Votre arme est rechargée";
+			} else
+			{
+				messageRecharge = "Votre arme n'est pas rechargée";
+			}
+
+			if (_ballReserve == 0 && _ballGun == 0) 
+			{ 
+				return "Vous avez raté votre évaluation, merci de cliquer sur [Q] pour quitter"; 
+			} else if (_ballGun == 0)
+			{
+				return "Vous êtes en train de courrir dans une prairie mais vous n'avez plus de balles dans votre chargeur, n'oubliez pas de le charger\n===================================================\n";
+            } else
+			{
+				return "Vous êtes en train de courrir dans une prairie\n===================================================\n  [+] Balles dans votre chargeur: " + _ballGun + "\n  [+] Balles de réserve: " + _ballReserve + "\n  [+] " + messageRecharge + "\n\n[ESPACE] Tirer\n[R] Recharger\n[Q] Quitter l'évaluation";
+			}
+        }
+
+
+
+	}
+}
---
+++ Poo/EvaluationCertificative12-12/CeUAA14Partie2_dec23_FranckenLouis/Program.cs
@@ -0,0 +1,98 @@
+﻿using CeUAA14Partie2_dec23_FranckenLouis;
+using System.Runtime.Intrinsics.Arm;
+using System.Security;
+using System.Security.Cryptography.X509Certificates;
+
+bool verif;
+int ballreservation;
+int capacite;
+bool quitter;
+string message;
+bool go;
+
+
+verif = false;
+quitter = false;
+capacite = 0;
+
+
+
+Console.WriteLine("Bonjour soldat,\n\nBienvenue sur le terrain d'entraînement militaire. Aujourd'hui, nous allons évaluer tes capacités en terme de gestion de cartouches ou encore de ton aim ainsi que tes mouvements. \n\nAvant de commencer, voici plusieurs armes que tu peux choisir, je te conseille de choisir celle par défaut... (Sélectionnez avec le NumPad) \n  [+] LittleGun (10/chargeur)\n  [+] NormalGun (12/chargeur)\n  [Par défaut] BigGun (16/chargeur)\n\nQue choississez-vous?");
+do
+{
+    ConsoleKeyInfo info = Console.ReadKey();
+    if (info.Key == ConsoleKey.NumPad1)
+    {
+        verif = true;
+        capacite = 10;
+    } else if (info.Key == ConsoleKey.NumPad2)
+    {
+        verif = true;
+        capacite = 12;
+    } else if ( info.Key == ConsoleKey.NumPad3)
+    {
+        verif = true; 
+        capacite = 16;
+    } else
+    {
+        verif = false;
+        Console.WriteLine("\nMerci de recommencer en sélectionnant 1-2-3 sur votre NumPad... \n");
+    }
+} while (verif == false);
+
+verif = false;
+Thread.Sleep(2000);
+Console.Clear();
+Console.WriteLine("Très bon choix ! \n\nA présent, combien de balle souhaitez-vous acheter pour les mettres dans votre poche (Minimum: 30)");
+do
+{
+    ballreservation = Convert.ToInt32(Console.ReadLine());
+    if (ballreservation >= 30)
+    {
+        verif = true;
+    } else
+    {
+        Console.WriteLine("\nMerci de recommencer correctement en indiquant un chiffre supérieur ou égale à 30");
+        verif = false;
+    }
+} while (verif == false);
+
+
+
+Thread.Sleep(2000);
+Console.Write("Préparation de votre arme en cours");
+    GunFiles Gun1 = new GunFiles(ballreservation, 30, false, capacite, ballreservation);
+for (int i = 0; i < 5; i++)
+{
+    Thread.Sleep(500);
+    Console.Write(".");
+}
+Console.WriteLine("\n\nVotre arme est prête, lancement du combat dans 5 secondes");
+Thread.Sleep(5000);
+Console.Clear();
+
+
+do
+{
+    message = Gun1.AffichageStuff;
+    Console.WriteLine(message);
+    ConsoleKeyInfo info = Console.ReadKey();
+    if (info.Key == ConsoleKey.Q)
+    {
+        quitter = true;
+    } else if (info.Key == ConsoleKey.Spacebar)
+    {
+        Gun1.Tirer;
+        quitter = false;
+    } else if (info.Key == ConsoleKey.Spacebar)
+    {
+        Gun1.Recharger;
+        quitter = false;
+    }
+
+
+
+} while (quitter == false);
+Console.Clear();
+Console.WriteLine("Merci d'avoir jouer, le programme s'éteint dans 3 secondes");
+Thread.Sleep(3000);
