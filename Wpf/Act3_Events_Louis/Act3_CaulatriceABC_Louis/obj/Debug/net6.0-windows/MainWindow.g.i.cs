78b1a17b53271d2bf5b20ff2948068e91de856f8
lo-franckenstein <115708090+lo-franckenstein@users.noreply.github.com>
2024-01-12 13:19:04 +0100

    Reset syncr

--- Wpf/Act3_Events_Louis/Act3_CaulatriceABC_Louis/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ Wpf/Act3_Events_Louis/Act3_CaulatriceABC_Louis/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -93,7 +93,7 @@ namespace Act3_CaulatriceABC_Louis {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Act3_CaulatriceABC_Louis;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Act3_CaulatriceABC_Louis;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- Wpf/Act3_Events_Louis/Act3_CaulatriceABC_Louis/obj/Debug/net6.0-windows/PageResultat.g.i.cs
+++ Wpf/Act3_Events_Louis/Act3_CaulatriceABC_Louis/obj/Debug/net6.0-windows/PageResultat.g.i.cs
@@ -69,7 +69,7 @@ namespace Act3_CaulatriceABC_Louis {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Act3_CaulatriceABC_Louis;component/pageresultat.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Act3_CaulatriceABC_Louis;V1.0.0.0;component/pageresultat.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\PageResultat.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
