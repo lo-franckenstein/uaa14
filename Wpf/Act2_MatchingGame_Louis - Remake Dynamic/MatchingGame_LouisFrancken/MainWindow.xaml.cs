95e1bfcca06900ff32c9d40dcad97295ba558457
lo-franckenstein <115708090+lo-franckenstein@users.noreply.github.com>
2024-01-18 22:51:20 +0100

    Ajout Travail sur les interfaces dynamiques

---
+++ Wpf/Act2_MatchingGame_Louis - Remake Dynamic/MatchingGame_LouisFrancken/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace MatchingGame_LouisFrancken
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        TextBlock derniereTBClique; // on va l’utiliser pour faire une référence à  la TextBlock sur laquelle on vient de cliquer
+
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            SetUpGame();
+
+        }
+        private void SetUpGame()
+        {
+
+            // Création du tableau
+            ColumnDefinition[] columnDef = new ColumnDefinition[4];
+            RowDefinition[] rowDef = new RowDefinition[4];
+            TextBlock[,] textCase = new TextBlock[4, 4];
+            for (int i = 0; i < 4; i++)
+            {
+                columnDef[i] = new ColumnDefinition();
+                grdMain.ColumnDefinitions.Add(columnDef[i]);
+                rowDef[i] = new RowDefinition();
+                grdMain.RowDefinitions.Add(rowDef[i]);
+            }
+
+            // Placement des cases
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    textCase[i, j] = new TextBlock();
+                    textCase[i, j].HorizontalAlignment = HorizontalAlignment.Center;
+                    textCase[i, j].VerticalAlignment = VerticalAlignment.Center;
+                    textCase[i, j].FontSize = 50;
+                    textCase[i, j].MouseDown += TextBlock_MouseDown;
+                    textCase[i, j].Text = "?";
+                    // Placer précisément
+                    Grid.SetRow(textCase[i, j], i);
+                    Grid.SetColumn(textCase[i, j], j);
+                    grdMain.Children.Add(textCase[i, j]);
+                }
+            }
+        }
+
+
+        // Evènement clique
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlockActif = sender as TextBlock;
+            textBlockActif.Text = "X";
+            derniereTBClique = textBlockActif;
+        }
+    }
+}
--- Wpf/Act4a_CreationDynamique_Louis/MainWindow.xaml.cs
+++ Wpf/Act4a_CreationDynamique_Louis/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Windows;
 using System.Windows.Automation.Peers;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -28,49 +29,85 @@ namespace Act4a_CreationDynamique_Louis
         public void preparationGrillage()
         {
             // Création du tableau
-            ColumnDefinition[] colDef = new ColumnDefinition[3];
-            for (int i = 0; i < 3; i++)
-            {
-                colDef[i] = new ColumnDefinition();
-            }
-
-            // Colonnes
-            for (int i = 0; i < 3; i++)
-            {
-                grdMain.ColumnDefinitions.Add(colDef[i]);
-            }
-
+            ColumnDefinition[] coldef = new ColumnDefinition[3];
             RowDefinition[] rowDef = new RowDefinition[3];
-
             for (int i = 0; i < 3; i++)
             {
+                coldef[i] = new ColumnDefinition();
                 rowDef[i] = new RowDefinition();
-            }
 
-            // Lignes
-            for(int i = 0;i < 3; i++)
-            {
+                grdMain.ColumnDefinitions.Add(coldef[i]);
                 grdMain.RowDefinitions.Add(rowDef[i]);
             }
 
+
+            // Placement de la première bande jaune
+            TextBlock txtFirst = new TextBlock();
+            txtFirst.Text = "TextBlock créé dynamiquement";
+            txtFirst.FontSize = 60;
+            txtFirst.FontWeight = FontWeights.Bold;
+            txtFirst.Foreground = Brushes.Red;
+            txtFirst.Background = Brushes.Yellow;
+            Grid.SetRow(txtFirst, 0);
+            Grid.SetColumnSpan(txtFirst, 3);
+            grdMain.Children.Add(txtFirst);
+
+
+
+
+            // Placement de la deuxième bande
+            Button[] suiteButton = new Button[3];
             for (int i = 0; i < 3; i++)
             {
-                tableau[i, 1] = new Button();
-                Thickness myThickness = new Thickness();
-                myThickness.Bottom = 20;
-                myThickness.Left = 20;
-                myThickness.Right = 20;
-                myThickness.Top = 20;
-                tableau[i, 1].Margin = myThickness;
-                tableau[i, 1].Content = "Bouton" + (i + 1);
-
+                suiteButton[i] = new Button();
+                suiteButton[i].Height = 80;
+                suiteButton[i].Width = 80;
+                suiteButton[i].Content = "BOUTON " + (i + 1);
+                Grid.SetColumn(suiteButton[i], i);
+                Grid.SetRow(suiteButton[i], 1);
+                grdMain.Children.Add(suiteButton[i]);
             }
 
-            TextBlock mainText = new TextBlock();
+            // Placement de la troisième bande
+            TextBlock txtSecond = new TextBlock();
+            txtSecond.Text = "infos:";
+            txtSecond.FontSize = 20;
+            txtSecond.FontWeight = FontWeights.Bold;
+            txtSecond.Foreground = Brushes.Black;
+            txtSecond.Background = Brushes.Yellow;
+            Grid.SetRow(txtSecond, 2);
+            Grid.SetColumnSpan(txtSecond, 2);
+            grdMain.Children.Add(txtSecond);
+            TextBlock txtThird = new TextBlock();
+            txtThird.Height = 50;
+            txtThird.Text = "J'attends vos infos";
+            txtThird.FontSize = 20;
+            txtThird.Foreground = Brushes.Black;
+            txtThird.Background = Brushes.White;
+            Grid.SetRow(txtThird, 2);
+            Grid.SetColumnSpan(txtThird, 2);
+            grdMain.Children.Add(txtThird);
+            ComboBox optionsDiff = new ComboBox();
+            optionsDiff.Items.Add("Information 1");
+            optionsDiff.Items.Add("Information 2");
+            optionsDiff.VerticalAlignment = VerticalAlignment.Center;
+            optionsDiff.HorizontalAlignment = HorizontalAlignment.Center;
+            optionsDiff.Height = 90;
+            optionsDiff.Width = 130;
+            Grid.SetRow(optionsDiff, 3);
+            Grid.SetColumn(optionsDiff, 3);
+            grdMain.Children.Add(optionsDiff);
 
-            grdMain.SetRow(mainText, 0);
-            grdMain.SetRow(mainText, 0);
-            grdMain.SetRowSpan(mainText, 3);
+
+
+
+
+
+
+
+
+
+
 
 
 
