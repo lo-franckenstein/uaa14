031e6bfb71a19f0f9c6d55ee2d9567e0b8a13b29
lo-franckenstein <115708090+lo-franckenstein@users.noreply.github.com>
2024-01-23 14:37:18 +0100

    Ajout du travail sur BDD

---
+++ Bdd/Act1BDD_PremierPas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+using System.Security.AccessControl;
+using System.Security;
+using Org.BouncyCastle.Crypto.Operators;
+using Act1BDD_PremierPas;
+
+DataSet donnees;
+DataSet infos;
+mesOutils mp = new mesOutils();
+
+Console.WriteLine("Liste de vos biens:\n\n");
+if(mp.ListingBiens(out donnees))
+{
+    string listeBiens = mp.AfficheDataBiens(donnees);
+    Console.WriteLine(listeBiens);
+} else
+{
+    Console.WriteLine("Un soucis est arrivé lors de la demande...");
+}
+
+
+
+
---
+++ Bdd/Act1BDD_PremierPas/mesOutils.cs
@@ -0,0 +1,60 @@
+﻿using System.Data;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+using System.Security.AccessControl;
+using System.Security;
+using Org.BouncyCastle.Crypto.Operators;
+
+namespace Act1BDD_PremierPas
+{
+    public struct mesOutils
+    {
+        public string connexionDataBase()
+        {
+            string connexionIp = "server=10.10.51.98;database=immo;port=3306;User Id=louis;password=root";
+            return connexionIp;
+        }
+
+
+        public bool ListingBiens(out DataSet infos)
+        {
+            bool ok = false;
+            MySqlConnection maConnection = new MySqlConnection(connexionDataBase());
+            string query = "";
+            try
+            {
+                maConnection.Open();
+                query = "SELECT * FROM biens;";
+
+                MySqlDataAdapter da = new MySqlDataAdapter(query, maConnection);
+                infos = new DataSet();
+                da.Fill(infos, "infoBiens");
+                if (infos.Tables[0].Rows.Count >= 1)
+                {
+                    ok = true;
+                }
+                maConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                throw;
+            }
+            return ok;
+        }
+
+        public string AfficheDataBiens(DataSet donnees)
+        {
+            string infos = "";
+            for (int i = 0; i < donnees.Tables[0].Rows.Count; i++)
+            {
+                infos += donnees.Tables[0].Rows[i]["bienId"].ToString() + " | " +
+                    donnees.Tables[0].Rows[i]["nom"].ToString() + " | " +
+                    donnees.Tables[0].Rows[i]["taille"].ToString() + " | " +
+                    donnees.Tables[0].Rows[i]["prix"].ToString() + " | " +
+                    donnees.Tables[0].Rows[i]["ville"].ToString() + " | ";
+            }
+            return infos;
+        }
+    }
+}
